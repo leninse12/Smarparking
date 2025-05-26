e2f6e433d33ffce327c55bbcedae3f6d3f4d92f8
Alberto Márquez <bbetoben122@gmail.com>
2025-05-25 19:32:06 -0500

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,14 +4,14 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBbD4IRdx5K7nVY4lU57u1mdgYpoJucOro",
-  authDomain: "prueba-c7272.firebaseapp.com",
-  projectId: "prueba-c7272",
-  storageBucket: "prueba-c7272.firebasestorage.app",
-  messagingSenderId: "532736619114",
-  appId: "1:532736619114:web:6b3c56071c4d41e7325013",
-  measurementId: "G-9F18F3PVH7",
-  databaseURL: "https://prueba-c7272-default-rtdb.firebaseio.com"
+  apiKey: "apikey",
+  authDomain: "prueba",
+  projectId: "prueba",
+  storageBucket: "firebase",
+  messagingSenderId: "id",
+  appId: "id",
+  measurementId: "id",
+  databaseURL: "https:"
 };
 
 if (!firebase.apps.length) {
